161b939b93fd2051141934c8d83435b7e90ab6b4
Joshua David Mclean Escaleras <joshuamc123456@gmail.com>
2022-03-22 22:09:34 -0600

    Create ventas.php

---
+++ pages/ventas.php
@@ -0,0 +1,20 @@
+<?php include 'templates/header_admin.php'; ?>
+<main class="app-content">
+    <div class="app-title">
+        <div>
+            <h1><i class="fas fa-store-alt"></i> Ventas de tickets</h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+            <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+            <li class="breadcrumb-item"><a href="#">Blank Page</a></li>
+        </ul>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="tile">
+                
+            </div>
+        </div>
+    </div>
+</main>
+<?php include 'templates/footer_admin.php'; ?>
